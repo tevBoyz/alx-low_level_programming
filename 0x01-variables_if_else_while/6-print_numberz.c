5eb3d3c7b6cfd64c2131a864f5d0f4abc4dd0188
Dawit <dawit.ttamiru@gmail.com>
2022-04-08 11:14:57 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - entry point
+*Return: 0 always
+*/
+int main(void)
+{
+int n;
+n = 48;
+while (n < 58)
+{
+putchar(n);
+n += 1;
+}
+putchar('\n');
+return (0);
+}
