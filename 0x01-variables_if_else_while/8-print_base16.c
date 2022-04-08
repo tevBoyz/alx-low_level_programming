6b8f92389652a4db84a31891f05a7afb1ed96feb
Dawit <dawit.ttamiru@gmail.com>
2022-04-08 11:35:53 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - entry point
+*Return: 0 always
+*/
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+putchar(n);
+for (n = 97; n < 103; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
