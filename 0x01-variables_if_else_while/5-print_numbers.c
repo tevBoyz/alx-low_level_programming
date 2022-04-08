2bca3e42bf0d813415d911488ed96b5f23ff5399
Dawit <dawit.ttamiru@gmail.com>
2022-04-08 11:03:26 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main -entry point
+*Retrun: 0 always
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+printf("%d",i);
+}
+printf("\n");
+return (0);
+}
