3544e1cadb2f1d2b3774aca51b9fdcb1457eb3c2
Dawit <dawit.ttamiru@gmail.com>
2022-04-08 11:51:27 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - entry point
+*Return: 0 always
+*/
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n < 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
