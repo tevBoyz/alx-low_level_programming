a7c57a586aaf84adcd1832097fb3afe63b98fbb0
Dawit <dawit.ttamiru@gmail.com>
2022-04-08 11:27:36 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - main entry
+*Return: 0 always
+*/
+int main(void)
+{
+int n;
+for (n = 122; n > 96; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
