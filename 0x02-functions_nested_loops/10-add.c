8fe339b171c735d3fb8296bb979a82a84aacbddd
Dawit <dawit.ttamiru@gmail.com>
2022-04-12 07:52:36 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add -  takes two integers and returns their sum
+ * @n: integer 1
+ * @m: integer 2
+ *
+ * Return: the sum
+ */
+
+int add(int n, int m)
+{
+return (n + m);
+}
+
