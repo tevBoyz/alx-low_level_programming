b521d06c9d14b88127e05ca74de5ce5ece4dc154
Dawit <dawit.ttamiru@gmail.com>
2022-04-12 08:33:39 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from a given num to 98
+ * @n: start number
+ *
+ * Return: no return val
+ */
+
+void print_to_98(int n)
+{
+int i;
+
+if (n < 98)
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d ,", i);
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d ,", i);
+}
+}
+printf("\n");
+}
