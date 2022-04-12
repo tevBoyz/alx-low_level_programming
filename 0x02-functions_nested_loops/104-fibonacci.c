1ccb1f3411071c8a159305b021b7e724407523ba
Dawit <dawit.ttamiru@gmail.com>
2022-04-12 12:00:24 -0700

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -32,7 +32,7 @@ for (count = 93; count < 99; count++)
 {
 half1 = fib1_half1 + fib2_half1;
 half2 = fib1_half2 + fib2_half2;
-if (fib1_half + fib2_half2 > 9999999999)
+if (fib1_half2 + fib2_half2 > 9999999999)
 {
 half1 += 1;
 half2 %= 10000000000;
