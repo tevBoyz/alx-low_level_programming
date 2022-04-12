816e9c687ad79024898907f29a466abacfb6fb7d
Dawit <dawit.ttamiru@gmail.com>
2022-04-12 09:59:15 -0700

    task 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main block to display the first 50 fibonacci sequence
+ * Return: 0  nothing
+ */
+
+int main(void)
+{
+int i, n, t1, t2, nextTerm;
+
+n = 50;
+t1 = 0;
+t2 = 1;
+
+nextTerm = t1 + t2;
+
+printf("%d, %d", t1, t2);
+
+for (i = 3; i <= n; i++)
+{
+printf("%d, ", nextTerm);
+t1 = t2;
+t2 = nextTerm;
+nextTerm = t1 + t2;
+}
+return (0);
+}
