e50c7469912e0c96d5e65577af954932ce4ea54b
Dawit <dawit.ttamiru@gmail.com>
2022-04-14 00:14:22 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14 10 times
+ *
+ * Return: nothing
+ */
+
+void more_numbers(void)
+{
+int i, j = 0;
+
+while (j < 10)
+{
+for (i = 0; i < 15; i++)
+{
+if (i < 10)
+_putchar(i + '0');
+else
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+}
+}
+_putchar('\n');
+j++;
+}
+}
