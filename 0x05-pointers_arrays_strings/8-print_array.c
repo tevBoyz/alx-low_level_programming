0bd77d2d68d4930cb035090c967e3c5efd820b19
Dawit <dawit.ttamiru@gmail.com>
2022-04-18 12:10:44 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints an array passed separated by comma
+ * @a: array to be printed
+ * @n: size of the array
+ *
+ * Return: none
+ */
+
+void print_array(int *a, int n)
+{
+int x;
+
+for (x = 0; x < n; x++)
+{
+if (x < n - 1)
+printf("%d, ", a[x]);
+else
+printf("%d\n", a[x]);
+}
+}
+
