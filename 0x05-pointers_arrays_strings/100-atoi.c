d2dde9332e228b58860fa65cba9ec045d24b7b93
Dawit <dawit.ttamiru@gmail.com>
2022-04-18 14:21:40 -0700

    task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,9 +28,9 @@ sign = sign * -1;
 
 while (s[i] >= '0' && s[i] <= '9')
 {
-if (result >= INT_MAX - 9 && sign == 1)
+if (result * 10 >= INT_MAX - 9 && sign == 1)
 return (INT_MAX);
-else if (result >= INT_MAX - 9 && sign == -1)
+else if (result * 10 >= INT_MAX - 9 && sign == -1)
 return (INT_MIN);
 
 result = result * 10 + s[i] - '0';
