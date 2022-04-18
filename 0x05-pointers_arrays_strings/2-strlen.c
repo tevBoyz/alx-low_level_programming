b248023de9dea39375767e142f684d5887f5994f
Dawit <dawit.ttamiru@gmail.com>
2022-04-18 03:22:02 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a given string
+ * @s: string given
+ *
+ * Return: string length int
+ */
+
+int _strlen(char *s)
+{
+int n;
+
+n = strlen(s);
+return (n);
+}
+
