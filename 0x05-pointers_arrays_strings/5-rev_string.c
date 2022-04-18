65f1eb25dcc3bfd2aea0e4c5b732f982e62f1856
Dawit <dawit.ttamiru@gmail.com>
2022-04-18 09:52:13 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ *
+ * Return: none
+ */
+
+void rev_string(char *s)
+{
+char c;
+int i, x, mid1, mid2, length = 0;
+
+for (i = 0; s[i]; i++)
+length++;
+
+if (length % 2 != 0)
+{
+mid1 = length / 2;
+}
+else
+{
+mid1 = length / 2;
+mid2 = mid1 - 2;
+
+c = s[mid1];
+*(s + mid1) = *(s + mid2);
+*(s + mid2) = c;
+}
+
+x = length;
+
+for (length = 0; length < mid1; length++)
+{
+c = s[length];
+*(s + length) = *(s + (x - length - 1));
+*(s + (x - length - 1)) = c;
+}
+}
