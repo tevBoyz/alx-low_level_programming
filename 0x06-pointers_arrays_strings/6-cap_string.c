6347d7be00088ee2c3b03f3c0a46541dd7b433b7
Dawit <dawit.ttamiru@gmail.com>
2022-04-21 12:13:25 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,23 +3,25 @@
 /**
  * cap_string - capitalized letters on a string with separator
  * {, ; . ! ? '' ( ) { } space tab and new line
- * @str: string to be capitalized
+ * @src: string to be capitalized
  *
  * Return: capitalized string pointer
  */
 
-char *cap_string(char *str)
+char *cap_string(char *src)
 {
 int x, y;
 int trigger;
-char nots[] = ",;.!?(){}\nt\" ";
+char nots[] = ",;.!?(){}\n\t\" ";
 
 for (x = 0, trigger = 0; src[x] != '\0'; x++)
 {
 if (src[0] > 96 && src[0] < 123)
-trigger = 1;
+{
+src[0] -= 32;
+}
 
-for (y = 0; nots[y] != '\0'; y++)
+for (y = 0; trigger == 0 && nots[y] != '\0'; y++)
 {
 if (nots[y] == src[x])
 trigger = 1;
@@ -27,14 +29,14 @@ trigger = 1;
 
 if (trigger)
 {
-if (src[x] > 96 && src[x] < 123)
+if (src[x + 1] > 96 && src[x + 1] < 123)
 {
-src[x] -= 32;
+src[x + 1] -= 32;
 trigger = 0;
 }
-else if (src[x] > 64 && src[x] < 91)
+else if (src[x + 1] > 64 && src[x + 1] < 91)
 trigger = 0;
-else if (src[x] > 47 && src[x] < 58)
+else if (src[x + 1] > 47 && src[x + 1] < 58)
 trigger = 0;
 }
 }
