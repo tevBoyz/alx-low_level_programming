0389233272e73ee787209e284e7e14270fcd1cf3
Dawit <dawit.ttamiru@gmail.com>
2022-04-21 08:16:32 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,20 +10,9 @@
 
 void reverse_array(int *a, int n)
 {
-int i, mid1, mid2, temp;
+int i, temp;
 
-if (n % 2 == 0)
-{
-mid2 = n / 2;
-mid1 = mid2 - 1;
-temp = a[mid1];
-a[mid1] = a[mid2];
-a[mid2] = temp;
-}
-else
-mid1 = n / 2;
-
-for (i = 0; i < mid1; i++)
+for (i = 0; i < n / 2; i++)
 {
 temp = a[i];
 a[i] = a[n - i - 1];
