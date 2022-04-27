00419d2653386749d2e04f6cb0c3f278a6651170
Dawit <dawit.ttamiru@gmail.com>
2022-04-27 11:12:22 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculates power funtion using recursion
+ * @x: base
+ * @y: exponent
+ *
+ * Return: product
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
+
