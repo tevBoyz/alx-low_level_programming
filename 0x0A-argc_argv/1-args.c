5a36f74a75eb7136992d0d2ebfff11d8b2d66832
Dawit <dawit.ttamiru@gmail.com>
2022-04-29 09:51:06 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: exit status
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+
+printf("%d\n", argc - 1);
+
+return (0);
+}
+
