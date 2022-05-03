85866668a8ddbe1207ed5fb73aa3be82e3108374
Dawit <dawit.ttamiru@gmail.com>
2022-05-03 09:46:00 -0700

    task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * free_grid - frees an occupied grid
+ * @grid: grid to be de allocated
+ * @height: height of grid
+ *
+ * Return: none
+ */
+
+void free_grid(int **grid, int height)
+{
+int i;
+
+for (i = 0; i < height; i++)
+free(grid[i]);
+
+free(grid);
+}
+
