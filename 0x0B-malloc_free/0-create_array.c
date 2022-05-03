61f44d06127a247b5bb931d95c29543fb4082d55
Dawit <dawit.ttamiru@gmail.com>
2022-05-03 05:28:32 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,9 @@ char *create_array(unsigned int size, char c)
 unsigned int i;
 char *res = (char *) malloc(sizeof(char) * size);
 
+if (size <= 0 || *res == '\0')
+return ('\0');
+
 for (i = 0; i < size; i++)
 {
 *(res + i) = c;
