81d7debad7609dbc6d7c9444318841deea886562
Dawit <dawit.ttamiru@gmail.com>
2022-05-03 13:12:47 -0700

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,101 +3,110 @@
 #include <stdio.h>
 
 /**
-*strtow - splits a stirng into words
-*@str: string to be splitted
-*
-*Return: pointer to the array of splitted words
-*/
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
 
+
+int _strlen(char *s)
+{
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
+
+char *str_addChar(char *str, char c)
+{
+int size, i;
+char *m;
+
+size = _strlen(str);
+
+m = malloc((size + 1) * sizeof(char) + 1);
+if (m == 0)
+return (0);
+
+for (i = 0; i <= size; i++)
+m[i] = str[i];
+
+m[i + 1] = c;
+m[i + 2] = '\0';
+
+return (m);
+}
+
+
+/**
+ * *nbr_spaces - return the number of occurent of a string
+ * @s: string to check
+ * Return: int
+ */
+
+unsigned int nbr_spaces(char *s)
+{
+int i, cmpt = 0;
+
+for (i = 0; s[i + 1] != '\0'; i++)
+{
+if (s[i]  == ' ' && s[i + 1] != ' ')
+cmpt++;
+}
+
+return (cmpt + 1);
+}
+
+
+/**
+  *strtow - split a sentence into multiple words.
+  *@str: the string passed as argument.
+  *Return: tokens
+  */
 char **strtow(char *str)
 {
-char **split;
-int i, j = 0, temp = 0, size = 0, words = num_words(str);
+int i;
+char **tokens = NULL;
+char *token;
+int checkingSpace = 0;
+int word = 0;
 
-if (words == 0)
-return (NULL);
-split = (char **) malloc(sizeof(char *) * (words + 1));
-if (split != NULL)
+if (!tokens)
 {
-for (i = 0; i <= len(str) && words; i++)
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-size++;
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-split[j] = (char *) malloc(sizeof(char) * size + 1);
-if (split[j] != NULL)
-{
-while (temp < size)
-{
-split[j][temp] = str[(i - size) +temp];
-temp++;
+printf("Failed");
+return (0);
 }
-split[j][temp] = '\0';
-size = temp = 0;
-j++;
+
+printf("looping");
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == ' ')
+{
+if (checkingSpace == 0)
+{
+word++;
+checkingSpace = 1;
+}
 }
 else
 {
-while (j-- >= 0)
-free(split[j]);
-free(split);
-return (NULL);
-}
-}
-}
-split[words] = NULL;
-return (split);
-}
-else
-return (NULL);
+printf("1");
+token = tokens[word];
+free(tokens[word]);
+str_addChar(token, str[i]);
+checkingSpace = 0;
 }
 
-
-/**
-* num_words - counts the number of words in str
-*@str: string to be used
-*
-*Return: number of words
-*/
-int num_words(char *str)
-{
-int i = 0, words = 0;
-
-while (i <= len(str))
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-{
-i++;
-}
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-words += 1;
-i++;
-}
-else
-{
-i++;
-}
-}
-return (words);
 }
 
-/**
-* len - returns length of str
-*@str: string to be counted
-*
-* Return: length of the string
-*/
+tokens[i] = NULL;
 
-int len(char *str)
-{
-int len = 0;
-
-if (str != NULL)
-{
-while (str[len])
-len++;
-}
-return (len);
+return (tokens);
 }
