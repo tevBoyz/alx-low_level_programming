2379620536d07eb33cb4593506a7e66f5eee9fbe
Dawit <dawit.ttamiru@gmail.com>
2022-05-09 07:21:12 -0700

    task 0

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-Object_like_macro.h
+ * Desc: Header file that defines a macro named SIZE
+ */
+
+#define SIZE 1024
+
+#endif
+
