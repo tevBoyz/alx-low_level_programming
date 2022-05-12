b442459748376778259eed3e32f9707839dbdd9c
Dawit <dawit.ttamiru@gmail.com>
2022-05-12 07:03:38 -0700

    task 0

---
+++ 0x10-variadac_functions.c/0-sum_them_all.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+
+/**
+ * sum_them_all - sum of all parameters
+ * @n: total number of parameters
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int sum = 0;
+
+va_list(args);
+
+if (n == 0)
+return (sum);
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+sum += va_arg(args, int);
+
+va_end(args);
+
+return (sum);
+}
+
