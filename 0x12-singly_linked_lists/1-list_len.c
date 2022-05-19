04b03d3cb4b92ce1ac01448ceb8b27ba75c0725d
Dawit <dawit.ttamiru@gmail.com>
2022-05-19 06:20:18 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,12 +7,12 @@
  * Return: length of linked list
  */
 
-size_t list_len(cont list_t *h)
+size_t list_len(const list_t *h)
 {
 size_t nodes = 0;
 const list_t *current = h;
 
-while (current != null)
+while (current != NULL)
 {
 current = current->next;
 nodes++;
