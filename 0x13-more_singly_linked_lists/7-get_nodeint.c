e93781b65a6502851a91c223620aad2dafcd051b
Dawit <dawit.ttamiru@gmail.com>
2022-05-23 10:01:16 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int iter = 0;
 listint_t *curr;
+
 if (head == NULL)
 return (0);
 
 curr = head;
 
-while (curr && curr->next != NULL && iter <= index)
+while (curr && iter < index)
 {
 if (iter == index)
 return (curr);
