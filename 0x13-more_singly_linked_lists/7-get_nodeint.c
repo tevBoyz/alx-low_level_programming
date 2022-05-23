f7ddf4b0ca2146133126f5fc54f9224f6bb9a761
Dawit <dawit.ttamiru@gmail.com>
2022-05-23 09:38:24 -0700

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns node at given index
+ * @head: head of list
+ * @index: index of node to return
+ *
+ * Return: the node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int iter = 0;
+listint_t *curr;
+
+curr = head;
+
+while (curr && curr->next != NULL && iter <= index)
+{
+if (iter == index)
+return (curr);
+
+iter++;
+curr = curr->next;
+}
+
+return (0);
+}
