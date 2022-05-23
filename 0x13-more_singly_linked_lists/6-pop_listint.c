92cd4cc1292f5c317d2e6ad49993e6fb4a09ba08
Dawit <dawit.ttamiru@gmail.com>
2022-05-23 09:12:14 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - returns the head datat and deletes it
+ * @head: head of list
+ *
+ * Return: head data
+ */
+
+int pop_listint(listint_t **head)
+{
+listint_t *curr;
+int x;
+
+if (head == NULL)
+return (0);
+
+curr = *head;
+x = curr->n;
+
+*head = curr->next;
+free(curr);
+
+return (x);
+}
+
