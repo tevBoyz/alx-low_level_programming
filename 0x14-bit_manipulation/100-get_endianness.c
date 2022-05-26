82eb65b114fab5db46989202e16a35827a2a8e01
Dawit <dawit.ttamiru@gmail.com>
2022-05-26 12:33:58 -0700

    task 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks endianneess
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+unsigned int i = 1;
+char *c = (char *) &i;
+
+
+return (*c);
+}
+
