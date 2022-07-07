1edb9818b1ced90e7a4a4ee461f9e881a55e86d7
Dawit <dawit.ttamiru@gmail.com>
2022-07-07 10:57:29 -0700

    task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,66 +1,52 @@
 #include "lists.h"
 
-/**
- * dlistint_len - counts the number of nodes
- * @h: head of list
- *
- * Return: count of nodes
- */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-size_t count = 0;
-const dlistint_t *temp;
-
-if (h == NULL)
-return (0);
-
-temp = h;
-
-while (temp != NULL)
-{
-temp = temp->next;
-count++;
-}
-return (count);
-}
 
 /**
- * delete_nodeint_at_index - delete a node at index
+ * delete_dnodeint_at_index - delete a node at index
  * @head: head of list
  * @index: index
  *
- * Return: 1 on success, 0 on failure
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-unsigned int idx = 0;
+unsigned int idx;
 dlistint_t *temp;
 
-temp = *head;
-if (*head == NULL || index > dlistint_len(temp))
+if (*head == NULL)
 return (-1);
-if (index == 0 && temp->next)
+
+temp = *head;
+if (index == 0)
+{
+*head = temp->next;
+if (temp->next != NULL)
 {
 temp->next->prev = NULL;
-*head = temp->next;
+}
 free(temp);
 return (1);
 }
-else if (index == 0 && temp)
-{
-free(temp);
-*head = NULL;
-return (1);
-}
-while (idx < index)
+for (idx = 0; temp != NULL && idx < index - 1; idx++)
 {
 temp = temp->next;
-idx++;
 }
-temp->prev->next = temp->next;
-if (temp->next)
-temp->next->prev = temp->prev;
-free(temp);
+if (temp == NULL || temp->next == NULL)
+{
+return (-1);
+}
+if (temp->next->next != NULL)
+{
+temp->next = temp->next->next;
+free(temp->next->prev);
+temp->next->prev = temp;
 return (1);
+}
+else
+{
+free(temp->next);
+temp->next = NULL;
+return (1);
+}
+return (-1);
 }
