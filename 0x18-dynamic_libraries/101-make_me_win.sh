c4a48ca6cb43bc46dfa632b9cd5eb2b939863386
Dawit <dawit.ttamiru@gmail.com>
2022-07-12 11:53:46 -0700

    task 101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp/ https://raw.githubusercontent.com/tevBoyz/alx-low_level_programming/master/0x18-dynamic_libraries/modlib.so
-export LD_PRELOAD="$PWD/tmp/modlib.so"
+export LD_PRELOAD=/tmp/modlib.so
